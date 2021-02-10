ddd70c3a1668ef06ce43b16a63d9342c1e7885f2
Liza Vasina <liz.vasina@gmail.com>
2021-02-11 00:35:49 +0300

    fix app js

--- src/components/App.js
+++ src/components/App.js
@@ -9,24 +9,24 @@ import React, {useState} from 'react';
 
 
 function App() {
-  const [isEditProfileOpen, setEditProfileOpen] = useState(false);
-  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = useState(false);
-  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
-  const [isSubmitPopupOpen, setSubmitPopupOpen] = useState(false);
-  const [isImagePopupOpen, setImagePopupOpen] = useState(false);
+  const [isEditProfileOpen, setIsEditProfileOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  const [isSubmitPopupOpen, setIsSubmitPopupOpen] = useState(false);
+  const [isImagePopupOpen, setIsImagePopupOpen] = useState(false);
   const [selectedCard, setSelectedCard] = useState(null);
 
   function handleCardClick(cardData) {
-    setImagePopupOpen(true);
+    setIsImagePopupOpen(true);
     setSelectedCard(cardData);
   }
 
   function CloseAllPopups() {
-    setEditProfileOpen(false);
-    setAddPlacePopupOpen(false);
-    setEditAvatarPopupOpen(false);
-    setSubmitPopupOpen(false);
-    setImagePopupOpen(false);
+    setIsEditProfileOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setIsSubmitPopupOpen(false);
+    setIsImagePopupOpen(false);
     setSelectedCard(null);
   }
 
@@ -39,13 +39,13 @@ function App() {
     
     <Main 
       onEditProfile={() => {
-        setEditProfileOpen(true);
+        setIsEditProfileOpen(true);
       }}
       onAddPlace={() => {
-        setAddPlacePopupOpen(true);
+        setIsAddPlacePopupOpen(true);
       }}
       onEditAvatar={() => {
-        setEditAvatarPopupOpen(true);
+        setIsEditAvatarPopupOpen(true);
       }}
       onCardClick={handleCardClick}
     />
@@ -58,13 +58,13 @@ function App() {
       formName='info'
       buttonName='save'
       buttonTitle='Сохранить'
-      children= {<div><input id="name" type="text" minLength="2" maxLength="40" autoComplete="off" placeholder="Имя" name="name" className="popup__text popup__text_type_name" required></input>
-      <span id="name-error" className="popup__text-error"></span>
-      <input id="description" type="text" minLength="2" maxLength="200" autoComplete="off" placeholder="О себе" name="description" className="popup__text popup__text_type_description" required></input>
-      <span id="description-error" className="popup__text-error"></span></div>}
       isOpen={isEditProfileOpen}
-      onClose={CloseAllPopups}
-    />
+      onClose={CloseAllPopups}>
+        <div><input id="name" type="text" minLength="2" maxLength="40" autoComplete="off" placeholder="Имя" name="name" className="popup__text popup__text_type_name" required></input>
+        <span id="name-error" className="popup__text-error"></span>
+        <input id="description" type="text" minLength="2" maxLength="200" autoComplete="off" placeholder="О себе" name="description" className="popup__text popup__text_type_description" required></input>
+        <span id="description-error" className="popup__text-error"></span></div>
+      </PopupWithForm>
 
     <PopupWithForm 
       name='photos'
@@ -72,13 +72,14 @@ function App() {
       formName='photos'
       buttonName='create'
       buttonTitle='Создать'
-      children= {<div><input id="pic-name" type="text" minLength="2" maxLength="30" placeholder="Имя" name="name" className="popup__text popup__text_type_pic-name" required></input>
-      <span id="pic-name-error" className="popup__text-error"></span>
-      <input id="link" type="url" placeholder="Ссылка на картинку" name="link" className="popup__text popup__text_type_link" required></input>
-      <span id="link-error" className="popup__text-error"></span></div>}
       isOpen={isAddPlacePopupOpen}
-      onClose={CloseAllPopups}
-    />
+      onClose={CloseAllPopups}>
+        <div><input id="pic-name" type="text" minLength="2" maxLength="30" placeholder="Имя" name="name" className="popup__text popup__text_type_pic-name" required></input>
+        <span id="pic-name-error" className="popup__text-error"></span>
+        <input id="link" type="url" placeholder="Ссылка на картинку" name="link" className="popup__text popup__text_type_link" required></input>
+        <span id="link-error" className="popup__text-error"></span></div>
+      </PopupWithForm>
+    
 
     <PopupWithForm 
       name='submit'
@@ -95,11 +96,11 @@ function App() {
       formName='avatar'
       buttonName='update'
       buttonTitle='Сохранить'
-      children= {<div><input id="avatar" type="url" placeholder="Ссылка на аватар" name="link" className="popup__text popup__text_type_link" required></input>
-      <span id="avatar-error" className="popup__text-error"></span></div>}
       isOpen={isEditAvatarPopupOpen}
-      onClose={CloseAllPopups}
-    />
+      onClose={CloseAllPopups}>
+        <div><input id="avatar" type="url" placeholder="Ссылка на аватар" name="link" className="popup__text popup__text_type_link" required></input>
+        <span id="avatar-error" className="popup__text-error"></span></div>
+      </PopupWithForm>
 
     <ImagePopup 
       card={selectedCard}
