b5e9fc77f2e71259ef71965d263abe04eeee52e4
Liza Vasina <liz.vasina@gmail.com>
2021-02-15 23:54:12 +0300

    feature: add user context to card'

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,19 @@
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 import React from 'react';
 
 function Card(props) {
+    const currentUser = React.useContext(CurrentUserContext);
+    const isOwn = props.card.owner._id === currentUser._id;
+    const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+    
+    const cardDeleteButtonClassName = (
+        `card__delete-button ${isOwn ? 'card__delete-button_visible' : 'card__delete-button_hidden'}`
+    )
+    const cardLikeButtonClassName = '...';
+
     return (
         <article className="card" >
-            <button type="button" className="card__delete-button"></button>
+            <button type="button" className={cardDeleteButtonClassName}></button>
             <button type="button" className="card__popup-button" onClick={() => props.onCardClick(props.card)}>
                 <img className="card__picture" src={`${props.card.link}`} alt={props.card.name}></img>
             </button>
